9e1da59b969c8d0b65cec12c4129f83e3d860281
Julie Montoux <92477225+JulieMontoux@users.noreply.github.com>
2024-03-13 15:15:10 +0100

    init project

---
+++ Imprimante/Imprimante/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Imprimante
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Imprimante/ImprimanteTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ImprimanteTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
